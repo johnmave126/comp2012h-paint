0ffd392448ef440ed25eb392f1ace4234d9b99ad
JohnMave126 <johnmave126@gmail.com>
2013-03-02 00:27:14 +0800

    Signed-off-by: JohnMave126 <johnmave126@gmail.com>

---
+++ src/paint_canvas_history.cpp
@@ -0,0 +1,49 @@
+/*
+ * Paint for COMP2012H
+ * Author: TAN, Shuhao
+ * Stu ID: 20090398
+ * 2013 Spring
+ *
+ * paint_canvas_history.cpp
+ *
+ * Handle affairs related to history operations
+ */
+
+#include "paint_canvas.h"
+#include <qwidget.h>
+#include <qapplication.h>
+#include <qpixmap.h>
+
+#include <list>
+using namespace std;
+
+void PaintCanvas::undo() {
+	//If now is begin, or no image loaded
+	//Do nothing
+	if(ImageHistory.empty() || Current == ImageHistory.begin()) {
+		return;
+	}
+	
+	//Undo, repaint and emit signal
+	Current--;
+	repaint();
+	emit undoabilityChanged(Current != ImageHisory.begin());
+	emit redoabilityChanged((Current+1) != ImageHisory.end());
+}
+
+void PaintCanvas::redo() {
+	//If now is end, or no image loaded
+	//Do nothing
+	if(ImageHistory.empty() || Current + 1 == ImageHistory.end()) {
+		return;
+	}
+	
+	//Undo, repaint and emit signal
+	Current++;
+	repaint();
+	emit undoabilityChanged(Current != ImageHisory.begin());
+	emit redoabilityChanged((Current+1) != ImageHisory.end());
+}
+
+void PaintCanvas::forward(QPixmap new_node) {
+}
