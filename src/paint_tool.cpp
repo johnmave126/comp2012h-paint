fac4eb0cf50440c17239fe7e7644e0bb2ff097db
JohnMave126 <johnmave126@gmail.com>
2013-03-08 03:16:31 +0800

    Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- src/paint_tool.cpp
+++ src/paint_tool.cpp
@@ -16,7 +16,7 @@
 #include <qpen.h>
 
 PaintTool::PaintTool(QMainWindow *parent)
-:drawPen(QColor(0, 0, 0)), my_parent(parent)
+:drawPen(QColor(0, 0, 0)), my_parent(parent),
  config_window(parent) {
 }
 
